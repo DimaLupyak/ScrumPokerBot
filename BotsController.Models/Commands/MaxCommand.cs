cd0d0069f11c145719d3e9445322a1c048dd1752
Dima Lupyak <dima.lupyak@gmail.com>
2019-10-30 16:59:10 +0200

    max command

--- BotsController.Models/Commands/MaxCommand.cs
+++ BotsController.Models/Commands/MaxCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -7,19 +8,27 @@ namespace BotsController.Models.Commands
 {
     public class MaxCommand : Command
     {
-        public override string Name => @"/макс";
+        public override string Name => @"/max";
 
         public override async Task ExecuteAsync(Message message, TelegramBotClient client)
         {
-            var stickerSet = await client.GetStickerSetAsync("More Squad");
-            foreach (var sticker in stickerSet.Stickers)
+            try
             {
-                await client.SendStickerAsync(message.Chat.Id,
-                    new InputOnlineFile(sticker.FileId)).ConfigureAwait(true);
-                await client.SendTextMessageAsync(message.Chat.Id, sticker.FileId);
-            }
+                var stickerSet = await client.GetStickerSetAsync("More Squad");
+                foreach (var sticker in stickerSet.Stickers)
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, sticker.FileId);
+                    await client.SendStickerAsync(message.Chat.Id,
+                        new InputOnlineFile(sticker.FileId)).ConfigureAwait(true);
+                    await client.SendTextMessageAsync(message.Chat.Id, sticker.FileId);
+                }
 
-            await client.SendTextMessageAsync(message.Chat.Id, "Аве Макс!!!");
+                await client.SendTextMessageAsync(message.Chat.Id, "Аве Макс!!!");
+            }
+            catch (Exception ex)
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, ex.ToString());
+            }
         }
     }
 }
