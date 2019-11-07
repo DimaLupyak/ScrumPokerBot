3d65e159ec0acfb98c9f2f2150dae4da0034de4e
Dmytro Lupiak <Dmytro.Lupiak@nice.com>
2019-11-07 17:15:29 +0200

    No Grisha damn

--- BotsController.Models/Commands/DamnCommand.cs
+++ BotsController.Models/Commands/DamnCommand.cs
@@ -1,56 +1,68 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BotsController.Core.Helpers;
+using BotsController.Models.Commands;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.InputFiles;
 
-namespace BotsController.Models.Commands
+namespace BotsController.Core.Commands
 {
     public class DamnCommand : Command
     {
         public override string Name => @"обізви";
-
-        public override Task ExecuteAsync(Message message, TelegramBotClient botClient)
+        private readonly string[] myNames = { "гріша", "гриша", "грішу", "гришу", };
+        public override async Task ExecuteAsync(Message message, TelegramBotClient botClient)
         {
             try
             {
-                string name = message.Text.ToLower().Replace(Name, string.Empty);
-                var text = GetDamn(name).Result;
+                var name = message.Text.ToLower().Replace(Name, string.Empty);
+                string text;
+                if (myNames.Any(grishaName => message.Text.ToLower().Contains(grishaName)) )
+                {
+                    text = "Пошел нахуй, не буду я себя обзывать. " + await GetDamn("ты").ConfigureAwait(false);
+                }
+                else
+                {
+                    text = await GetDamn(name).ConfigureAwait(false);
+                }
                 var speechGenerator = new SpeechGenerator();
-                using var stream = new MemoryStream(speechGenerator.SynthesizeSpeech(text));
+                await using var stream = new MemoryStream(speechGenerator.SynthesizeSpeech(text));
                 stream.Seek(0, SeekOrigin.Begin);
-                return botClient.SendAudioAsync(message.Chat.Id, new InputOnlineFile(stream, text), text);
+                await botClient.SendAudioAsync(message.Chat.Id, new InputOnlineFile(stream, text), text).ConfigureAwait(true);
             }
             catch (Exception ex)
             {
-                return botClient.SendTextMessageAsync(message.Chat.Id, "Не буду обзиватись, сьогодні я хороший.");
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Не буду обзиватись, сьогодні я хороший.").ConfigureAwait(true);
             }
         }
 
         static async Task<string> GetDamn(string name)
         {
             using var client = new HttpClient();
-            using var response = await client.GetAsync("https://damn.ru/?name=" + name + "&sex=m");
+            using var response = await client.GetAsync("https://damn.ru/?name=" + name + "&sex=m").ConfigureAwait(true);
             using var content = response.Content;
-            var myContent = await content.ReadAsStringAsync();
+            var myContent = await content.ReadAsStringAsync().ConfigureAwait(true);
             return GetDamnFromResponse(myContent);
         }
 
-        private static String GetDamnFromResponse(String s)
+        private static string GetDamnFromResponse(string s)
         {
             if (!s.Contains("<div class=\"damn\""))
                 return null;
-            var start = s.IndexOf("<div class=\"damn\"");
-            start = s.IndexOf(">", start + 1);
+            var start = s.IndexOf("<div class=\"damn\"", StringComparison.Ordinal);
+            start = s.IndexOf(">", start + 1, StringComparison.Ordinal);
             start++;
-            var end = s.IndexOf("</div", start);
+            var end = s.IndexOf("</div", start, StringComparison.Ordinal);
             var withSpan = s.Substring(start, end - start);
             withSpan = withSpan.Replace("<span class=\"name\">", "");
             withSpan = withSpan.Replace("&mdash; ", "");
             return withSpan.Replace("</span>", "");
         }
+
+
     }
 }
