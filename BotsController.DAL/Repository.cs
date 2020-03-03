92567cdcc766544ad1ab57bc406c6d7f0ed875bb
Dima Lupyak <dima.lupyak@gmail.com>
2020-03-03 23:31:30 +0200

    ашчуі

--- BotsController.DAL/Repository.cs
+++ BotsController.DAL/Repository.cs
@@ -22,7 +22,7 @@ namespace BotsController.DAL
         {
             var pidar = new Pidar()
             {
-                DateTime = DateTime.Now.ToShortTimeString(),
+                DateTime = DateTime.Now.ToString(),
                 UserName = userName
             };
             var dino = await firebase
